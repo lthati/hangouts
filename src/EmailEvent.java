30b05a9b589ca81034dc332fb524edd0a25097e8
sabhatia <sabhatia@gmail.com>
2017-10-24 15:05:54 -0700

    Initial Commit of Event Details Class

---
+++ src/EmailEvent.java
@@ -0,0 +1,13 @@
+/* Class that takes and event and sends an email to the group-mailer */
+import java.io.*;
+
+public class EmailEvent {
+
+    /* Temporary Event */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/EventDetails.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class EventDetails {
+
+    uint32 eventId;
+    String eventHeading;
+    String eventDescription;
+    String organizerName;
+    String eventLocation;
+    Date startTime;
+    Date endTime;
+
+}
